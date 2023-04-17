57b97bfea9c80eda40056cb08b6c384e679ef8b5
Erinle Oluwakorede <erinlekorede@gmail.com>
2023-04-17 22:49:12 +0100

    main

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -5,8 +5,8 @@ const sh = require('shelljs');
 
 module.exports = function renderAssets() {
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
-    const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
-    const imagePath = upath.resolve(upath.dirname(__filename), '../dist/assets/images.');
+    const destPath = upath.resolve(upath.dirname(__filename), '../dist/');
+    const imagePath = upath.resolve(upath.dirname(__filename), '../dist/assets/images');
     
     sh.cp('-R', sourcePath, destPath, imagePath)
 };
